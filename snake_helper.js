4642b95094e585e737dc489699337cffc755e2da
Michael Stark <mike.stark@vidyard.com>
2019-11-28 16:07:38 -0500

    fix getSnake

--- snake_helper.js
+++ snake_helper.js
@@ -1,6 +1,6 @@
 const getSnake = (state, snakeId)  => {
   if (!snakeId) snakeId = state.you.id;
-  for (let snake of state.snakes.data) {
+  for (let snake of state.board.snakes) {
     if (snake.id == snakeId) return snake;
   }
 }
